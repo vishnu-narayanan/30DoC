716531fd81bd338543174efd22cd86d20a9db42c
Vishnu Narayanan <iamwishnu@gmail.com>
2020-05-25 23:44:40 +0530

    Add day 2 in python

---
+++ 2/2.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+    total = meal_cost + meal_cost * (tip_percent+tax_percent)/100
+    total = int(round(total))
+    print(total)
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
+
